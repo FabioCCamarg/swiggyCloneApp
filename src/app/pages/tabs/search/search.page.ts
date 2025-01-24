a7730aaabdbe264e96b24e96b116679b8cd057b0
Fabio Costa <costafabiocamargo@gmail.com>
2025-01-24 16:16:45 -0300

    delivery for food

---
+++ src/app/pages/tabs/search/search.page.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
+  standalone: false
+})
+export class SearchPage implements OnInit {
+
+  @ViewChild('searchInput') sInput;
+  isLoading: boolean;
+  query: any;
+  allRestaurants: any[] = [
+    {
+      cover: 'assets/imgs/1.jpg',
+      name: 'Stayfit',
+      short_name: 'stayfit',
+      cuisines: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      // distance: 2.5,
+      price: 100,
+    },
+    {
+      cover: 'assets/imgs/2.jpg',
+      name: 'Stayfit1',
+      short_name: 'stayfit1',
+      cuisines: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      // distance: 2.5,
+      price: 100,
+    },
+    {
+      cover: 'assets/imgs/3.jpg',
+      name: 'Stayfit2',
+      short_name: 'stayfit2',
+      cuisines: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      // distance: 2.5,
+      price: 100,
+    },
+  ];
+
+  restaurants: any[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout( () => {
+      this.sInput.setFocus();
+    }, 500)
+  };
+
+  onSearchChange(event) {
+    console.log(event.detail.value);
+    this.query = event.detail.value.toLowerCase();
+    if(this.query.length > 0) {
+      this.restaurants = this.allRestaurants.filter((element: any) => {
+        return element.short_name.includes(this.query);
+      });
+      console.log(this.restaurants);
+    }
+  }
+
+
+}
